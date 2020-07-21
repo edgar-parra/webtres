9c39d90c62a232afa04c2097d7100fe2f78d2e2c
Edgar Parra <edgar_parra@hotmail.com>
2020-07-21 18:39:06 -0400

    Add api routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('users', 'UserController')->except('update');
+Route::post('users/{user}', 'UserController@update')->name('users.update');
